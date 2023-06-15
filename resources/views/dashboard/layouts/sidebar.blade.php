00c15cfb556bbb1533a24262cee4e7494a64a442
Ali Alqrinawi <104166368+AliAlqrinawi@users.noreply.github.com>
2023-06-15 20:00:49 +0300

    'cou'

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -217,33 +217,24 @@
                         src="https://img.icons8.com/?size=512&id=MG8AizwwWSHp&format=png&color=1A6DFF,C822FF" />
                     <span class="side-menu__label">{{ __('Videos') }}</span></a>
                 </li>
-                {{-- <li class="slide">
-                    <a class="side-menu__item" href="{{ route('coupon.index') }}"><svg xmlns="http://www.w3.org/2000/svg"
-                            class="side-menu__icon" viewBox="0 0 24 24">
-                            <path d="M0 0h24v24H0V0z" fill="none" />
-                            <path d="M5 5h4v6H5zm10 8h4v6h-4zM5 17h4v2H5zM15 5h4v2h-4z" opacity=".3" />
-                            <path
-                                d="M3 13h8V3H3v10zm2-8h4v6H5V5zm8 16h8V11h-8v10zm2-8h4v6h-4v-6zM13 3v6h8V3h-8zm6 4h-4V5h4v2zM3 21h8v-6H3v6zm2-4h4v2H5v-2z" />
-                        </svg><span class="side-menu__label">{{ __('Coupons') }}</span></a>
-                </li> --}}
-                {{-- <li class="slide">
-                    <a class="side-menu__item" href="{{ route('package.index') }}"><svg xmlns="http://www.w3.org/2000/svg"
-                            class="side-menu__icon" viewBox="0 0 24 24">
-                            <path d="M0 0h24v24H0V0z" fill="none" />
-                            <path d="M5 5h4v6H5zm10 8h4v6h-4zM5 17h4v2H5zM15 5h4v2h-4z" opacity=".3" />
-                            <path
-                                d="M3 13h8V3H3v10zm2-8h4v6H5V5zm8 16h8V11h-8v10zm2-8h4v6h-4v-6zM13 3v6h8V3h-8zm6 4h-4V5h4v2zM3 21h8v-6H3v6zm2-4h4v2H5v-2z" />
-                        </svg><span class="side-menu__label">{{ __('Packages') }}</span></a>
-                </li> --}}
-                {{-- <li class="slide">
-                    <a class="side-menu__item" href="{{ route('layout.index') }}"><svg xmlns="http://www.w3.org/2000/svg"
-                            class="side-menu__icon" viewBox="0 0 24 24">
-                            <path d="M0 0h24v24H0V0z" fill="none" />
-                            <path d="M5 5h4v6H5zm10 8h4v6h-4zM5 17h4v2H5zM15 5h4v2h-4z" opacity=".3" />
-                            <path
-                                d="M3 13h8V3H3v10zm2-8h4v6H5V5zm8 16h8V11h-8v10zm2-8h4v6h-4v-6zM13 3v6h8V3h-8zm6 4h-4V5h4v2zM3 21h8v-6H3v6zm2-4h4v2H5v-2z" />
-                        </svg><span class="side-menu__label">{{ __('Layouts') }}</span></a>
-                </li> --}}
+                <li class="slide">
+                    <a class="side-menu__item" href="{{ route('coupon.index') }}">
+                    <img width="30" style="margin-inline-end: 14px;"
+                    src="https://img.icons8.com/?size=512&id=46089&format=png&color=1A6DFF,C822FF" />
+                    <span class="side-menu__label">{{ __('Coupons') }}</span></a>
+                </li>
+                <li class="slide">
+                    <a class="side-menu__item" href="{{ route('package.index') }}">
+                        <img width="30" style="margin-inline-end: 14px;"
+                        src="https://img.icons8.com/?size=512&id=46106&format=png&color=1A6DFF,C822FF" />
+                    <span class="side-menu__label">{{ __('Packages') }}</span></a>
+                </li>
+                <li class="slide">
+                    <a class="side-menu__item" href="{{ route('layout.index') }}">
+                    <img width="30" style="margin-inline-end: 14px;"
+                    src="https://img.icons8.com/?size=512&id=48257&format=png&color=1A6DFF,C822FF" />
+                    <span class="side-menu__label">{{ __('Layouts') }}</span></a>
+                </li>
                 <li class="slide">
                     <a class="side-menu__item" data-bs-toggle="slide" href="javascript:void(0);">
                         <img class="side-menu__icon mCS_img_loaded" style=" width: 30px; height: 30px;"
